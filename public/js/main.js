50f56d74b296b894d0b0c063a0581501156c134e
Animesh Sharma <animeshs@mindfiresolutions.com>
2020-01-14 12:12:20 +0530

    working on same browser across different tabs

---
+++ public/js/main.js
@@ -0,0 +1,349 @@
+'use strict';
+
+var socket = io();
+
+// storing the local html dom video elements in constants.
+const localVideo = document.getElementById('localvideo');
+const remoteVideo = document.getElementById('remotevideo');
+
+let localStream;
+let remoteStream;
+var localPeerConnection;
+
+const servers = {
+    'iceServers': [
+    {
+      'url': 'stun:stun.l.google.com:19302'
+    }
+    // ,
+    // {
+    //   'url': 'turn:192.158.29.39:3478?transport=udp',
+    //   'credential': 'JZEOEt2V3Qb0y27GRntt2u2PAYA=',
+    //   'username': '28224511:1379330808'
+    // },
+    // {
+    //   'url': 'turn:192.158.29.39:3478?transport=tcp',
+    //   'credential': 'JZEOEt2V3Qb0y27GRntt2u2PAYA=',
+    //   'username': '28224511:1379330808'
+    // }
+    ]
+};
+
+// storing button elements..
+const callButton = document.getElementById('call');
+const endButton = document.getElementById('end');
+const usernameButton = document.getElementById('usernamesubmit');
+
+
+// setting media stream constraints to pass as an argument to getUserMedia
+const mediaStreamConstraints = {
+	video:true,
+	audio:true,
+};
+
+// showing video stream to local video element on clicking start button..
+$('#start').click((e)=>{
+	e.preventDefault();
+	navigator.mediaDevices.getUserMedia(mediaStreamConstraints)
+	.then(gotLocalMediaStream).catch(handleLocalMediaStreamError);
+	trace('Requesting local stream.');
+})
+// callback for get local user media stream
+function gotLocalMediaStream(stream) {
+	localVideo.srcObject = stream;
+    localStream = stream;
+    trace('Received Local stream..');
+}
+// error handling function for getting local media stream..
+function handleLocalMediaStreamError(error) {
+    trace(`navigator.getUserMedia error: ${error.toString()}.`);
+}
+
+function gotRemoteMediaStream(event) {
+    const mediaStream = event.stream;
+    remoteVideo.srcObject = mediaStream;
+    remoteStream = mediaStream;
+    trace('Remote peer connection received remote stream.');
+}
+
+
+// bind click event on set username button.. (function definition for setting username)
+$('#usernamesubmit').click((e)=>{
+	e.preventDefault();
+	var username = $('#username').val();
+	trace('setting username '+username);
+	socket.emit('new user', {name: username}, (data)=>{
+		//callback function definition..
+		if(data == 'success'){
+			$('#displayusername').html('username: '+ $('#username').val().toLowerCase().trim());
+			$('#username').val('');
+		}
+		else{
+			alert(data);
+		}
+	});
+});
+
+
+
+// bind click event on call button.. (function definition for call button)
+$('#call').click((e)=>{
+	e.preventDefault();
+	var callee = $('#callee').val();
+	socket.emit('call request', {callee: callee}, (data)=>{
+		if(data == 'success'){
+			trace('Starting call.');
+            var startTime = window.performance.now();
+
+  			//Get local media stream tracks.
+            const videoTracks = localStream.getVideoTracks();
+            const audioTracks = localStream.getAudioTracks();
+            if (videoTracks.length > 0) {
+               trace(`Using video device: ${videoTracks[0].label}.`);
+            }
+            if (audioTracks.length > 0) {
+               trace(`Using audio device: ${audioTracks[0].label}.`);
+            }
+
+  			// Create peer connections and add behavior.
+  			localPeerConnection = new RTCPeerConnection(servers);
+  			trace('Created local peer connection object localPeerConnection.');
+  			localPeerConnection.addEventListener('icecandidate', handleConnection);
+  			localPeerConnection.addEventListener(
+                  'iceconnectionstatechange', handleConnectionChange);
+            localPeerConnection.addEventListener('addstream', gotRemoteMediaStream);
+
+            socket.emit('call button clicked', {callee: callee});
+
+
+    		// adding local stream to local peer connection..
+    		localPeerConnection.addStream(localStream);
+    		trace('Added local stream to localPeerConnection.');
+            trace('localPeerConnection createOffer start.');
+
+  			// setting offer options..
+  			const offerOptions = {
+  				offerToReceiveVideo: 1,
+  				offerToReceiveAudio: 1,
+            };
+            // creating offer with local peer conection..
+            localPeerConnection.createOffer(offerOptions)
+            .then(createdOffer).catch(setSessionDescriptionError);
+
+
+
+
+
+        }
+        else{
+            alert(data);
+        }
+    });
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function handleConnection(event){
+	var iceCandidate = event.candidate;
+	if(iceCandidate){
+		const newIceCandidate = new RTCIceCandidate(iceCandidate);
+        let callee = $('#callee').val();
+        socket.emit('add ice candidate', {callee: callee, iceCandidate: JSON.stringify(newIceCandidate)});
+        trace('INSIDE handleConnection');
+    }
+}
+function handleConnection2(event){
+	var iceCandidate = event.candidate;
+	if(iceCandidate){
+		const newIceCandidate = new RTCIceCandidate(iceCandidate);
+		socket.emit('add ice candidate', {callee: window.caller, iceCandidate: JSON.stringify(newIceCandidate)});
+	}
+    trace('INSIDE handleConnection2');
+}
+
+function handleConnectionChange(event) {
+    const peerConnection = event.target;
+    console.log('ICE state change event: ', event);
+    // trace(`${getPeerName(peerConnection)} ICE state: ` +
+    //  `${peerConnection.iceConnectionState}.`);
+    trace('INSIDE handleConnectionChange');
+}
+
+function createdOffer(description){
+	trace(`Offer from localPeerConnection:\n${description.sdp}`);
+    trace('localPeerConnection setLocalDescription start.');
+    localPeerConnection.setLocalDescription(description);
+    // .then(() => {
+    //  	setLocalDescriptionSuccess(localPeerConnection);
+    // }).catch(setSessionDescriptionError);
+    // console.log(callee)
+    let callee = $('#callee').val();
+
+socket.emit('local description', {callee: callee, description: JSON.stringify(description)});
+trace('INSIDE createdOffer');
+}
+
+function createdAnswer(description){
+	//trace(`Answer from remotePeerConnection:\n${description.sdp}.`);
+
+   trace('remotePeerConnection setLocalDescription start.');
+   localPeerConnection.setLocalDescription(description);
+    // .then(() => {
+    //   setLocalDescriptionSuccess(localPeerConnection);
+    // }).catch(setSessionDescriptionError);
+
+    socket.emit('answer', {caller: window.caller, description: JSON.stringify(description)});
+    trace('INSIDE createdAnswer');
+}
+
+
+
+// Logs error when setting session description fails.
+function setSessionDescriptionError(error) {
+  trace(`Failed to create session description: ${error.toString()}.`);
+}
+
+// Logs success when setting session description.
+function setDescriptionSuccess(peerConnection, functionName) {
+  const peerName = getPeerName(peerConnection);
+  trace(`${peerName} ${functionName} complete.`);
+}
+
+// Logs success when localDescription is set.
+function setLocalDescriptionSuccess(peerConnection) {
+  setDescriptionSuccess(peerConnection, 'setLocalDescription');
+}
+
+// Logs success when remoteDescription is set.
+function setRemoteDescriptionSuccess(peerConnection) {
+  setDescriptionSuccess(peerConnection, 'setRemoteDescription');
+}
+
+
+
+
+
+
+
+
+
+
+
+
+// bind click event on end button.. (function definition for end button)
+$('#end').click((e)=>{
+	e.preventDefault();
+	localVideo.srcObject = null;
+	trace('call ended');
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+socket.on('call button clicked', (obj)=>{
+	window.caller2 = obj.caller;
+	trace('Starting call.');
+	var startTime = window.performance.now();
+
+	// Get local media stream tracks.
+  const videoTracks = localStream.getVideoTracks();
+  const audioTracks = localStream.getAudioTracks();
+  if (videoTracks.length > 0) {
+     trace(`Using video device: ${videoTracks[0].label}.`);
+ }
+ if (audioTracks.length > 0) {
+     trace(`Using audio device: ${audioTracks[0].label}.`);
+ }
+
+	// Create peer connections and add behavior.
+	localPeerConnection = new RTCPeerConnection(servers);
+  trace('LOCAL PEER CONNECTION::::'+localPeerConnection);
+  localPeerConnection.addEventListener('icecandidate', handleConnection2);
+  localPeerConnection.addEventListener(
+     'iceconnectionstatechange', handleConnectionChange);
+  localPeerConnection.addEventListener('addstream', gotRemoteMediaStream);
+
+	// adding local stream to local peer connection..
+	localPeerConnection.addStream(localStream);
+	trace('Added local stream to localPeerConnection.');
+	trace('localPeerConnection createOffer start.');
+
+});
+
+
+// create rtc peer connection object as local peer connection and add ice candidate..
+socket.on('add ice candidate', (obj)=>{
+   localPeerConnection.addIceCandidate(JSON.parse(obj.iceCandidate));
+   trace('ADD ICE CANDIDATE EMITTED');
+});
+
+// setting remote description on call request..
+socket.on('local description', (obj)=>{
+	window.caller = obj.caller;
+	localPeerConnection.setRemoteDescription(JSON.parse(obj.description))
+    .then(() => {
+      setRemoteDescriptionSuccess(localPeerConnection);
+  }).catch(setSessionDescriptionError);
+
+    // create answer..
+    trace('remotePeerConnection createAnswer start.');
+    localPeerConnection.createAnswer()
+    .then(createdAnswer)
+    .catch(setSessionDescriptionError);
+});
+
+socket.on('answer', (obj)=>{
+	trace('localPeerConnection setRemoteDescription start.');
+   localPeerConnection.setRemoteDescription(JSON.parse(obj.description))
+   .then(() => {
+      setRemoteDescriptionSuccess(localPeerConnection);
+  }).catch(setSessionDescriptionError);
+});
+
+
+
+
+
+
+
+
+
+// Logs an action (text) and the time when it happened on the console.
+function trace(text){
+	text = text.trim();
+	const now = (window.performance.now() / 1000).toFixed(3);
+	console.log(now, text);
+}
---
+++ server.js
@@ -0,0 +1,79 @@
+var express = require('express');
+var fs = require('fs');
+var app = express();
+var http = require('http').createServer(app);
+var io = require('socket.io')(http);
+var users = {};
+app.use(express.static('public'));
+
+
+// serving index.html on request to homepage..
+app.get('/', function(req, res){
+  res.sendFile(__dirname + '/index.html');
+});
+
+// listening on port 3000..
+http.listen(3000, function(){
+  console.log('listening on *:3000');
+});
+
+// handling connection event..
+io.on('connection', (socket)=>{
+
+	socket.on('call button clicked', (obj)=>{
+		console.log('call button clicked emitted by: '+socket.username+' to: '+obj.callee);
+		users[obj.callee].emit('call button clicked', {caller: socket.username});
+	})
+
+	socket.on('new user', (obj, callback)=>{
+		obj.name = obj.name.toLowerCase().trim();
+		if(socket.username){
+			callback('You already have a username: '+ socket.username);
+			return;
+		}
+		if(obj.name == ''){
+			callback('Where have you seen an empty username?');
+			return;
+		}
+		if(obj.name in users){
+			callback('Be Unique.. This one already exists');
+			return;
+		}
+		socket.username = obj.name;
+		users[obj.name] = socket;
+		console.log(socket.username+' joined..');
+		callback('success');
+		io.emit('users online', Object.keys(users));
+	});
+
+	socket.on('call request', (obj, callback)=>{
+		console.log('call request emmitted by: '+socket.username+' to: '+obj.callee);
+		if(!(socket.username in users)){
+			callback('Get a username first..');
+			return;
+		}
+		if(!(obj.callee in users)){
+			callback('This user does not exist');
+			return;
+		}
+		callback('success');
+	});
+
+	socket.on('add ice candidate', (obj)=>{
+		console.log('add ice candidate emmitted by: '+socket.username+' to: '+obj.callee);
+		var soc = users[obj.callee];
+		soc.emit('add ice candidate', {iceCandidate: obj.iceCandidate});
+	});
+
+	socket.on('local description', (obj)=>{
+		console.log('local description emmitted by: '+socket.username+' to: '+obj.callee);
+		var soc1 = users[obj.callee];
+		soc1.emit('local description', {caller: socket.username, description: obj.description});
+	});
+
+	socket.on('answer', (obj)=>{
+		console.log('answer emmitted by: '+socket.username+' to: '+obj.caller);
+		soc2 = users[obj.caller];
+		soc2.emit('answer', {description: obj.description});
+	});
+});
